268fe0a6e94243842071678525895573a3c7d46d
Lucas DRAESCHER <draescherl@cy-tech.fr>
2021-12-02 15:20:23 +0100

    Open graphs from files

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ import math
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
+import json
+
+
+with open('./15_vertices.json') as f:
+  graph_as_json = json.load(f)['graph']
+  original_graph = nx.from_numpy_matrix(np.array(graph_as_json))
+
+nx.draw(original_graph)
+plt.show()
