ba44b328d763738c82883de4e4a040942bcda634
Lucas Draescher <draescherl@eisti.eu>
2021-12-03 09:00:34 +0100

    Triangulated matrix

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import json
+import numpy as np
 
 from src.functions import simulated_annealing, visualise_path
 
 
 with open('./inputs/15_vertices.json') as f:
     json_data = json.load(f)
-    graph = json_data['graph']
+    graph = np.triu(json_data['graph'])
     best_path = json_data['best_path_value']
 
 best_path = simulated_annealing(graph, best_path)
-#visualise_path(graph, best_path)
+visualise_path(graph, best_path)
